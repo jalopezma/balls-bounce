e6afdaee0c0dce7f1a0d79ca33d3b04ae8f6f03d
Jose Lopez <jose@stratagem.co>
2018-05-23 12:34:29 +0100

    Import and export flex-layout on shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './modules/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   imports: [
     CommonModule,
     BrowserAnimationsModule,
     MaterialModule,
+    FlexLayoutModule,
   ],
   declarations: [],
   exports: [
     CommonModule,
     BrowserAnimationsModule,
     MaterialModule,
+    FlexLayoutModule,
   ],
 })
 export class SharedModule { }
